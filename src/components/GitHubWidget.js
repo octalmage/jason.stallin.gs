0661e0c66487f8be5d858a3c2352c7c73baf41e7
Jason Stallings <jason@stallin.gs>
2017-10-15 15:11:34 -0500

    Apparently all of this was unnecessary.

--- src/components/GitHubWidget.js
+++ src/components/GitHubWidget.js
@@ -1,11 +1,6 @@
 /* global GithubRepoWidget */
 import React from 'react';
-
-// Include this file on the page.
-if (typeof window !== 'undefined') {
-  // This line is all kinds of bad, but we need to import this for the GibHub Shortcode plugin.
-  require('!babel!github-repo-widget.js/GithubRepoWidget.min.js'); // eslint-disable-line
-}
+import 'github-repo-widget.js';
 
 const GitHubWidgetComponent = Component =>
   class GitHubWidget extends React.PureComponent {
