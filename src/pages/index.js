e7c1ca174b2fb5a9cdbb6a6ae1603ffe8b31f076
Jason Stallings <jason@stallin.gs>
2018-01-03 19:16:58 -0600

    Fix homepage image.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import LastGitHubProject from '../components/LastGitHubProject';
+import me from '../assets/stallings.jpg';
 
 const Home = ({ data }) =>
   (
     <div>
       <Helmet title={`${data.site.siteMetadata.title} | ${data.site.siteMetadata.subtitle}`} />
-      <div dangerouslySetInnerHTML={{ __html: data.wordpressPage.content }} />
+      <img src={me} alt="Me at a computer" />
+      <div>
+        <p>Hey, I’m Jason.</p>
+        <p>I live in Austin, Texas, and I work at <a href="http://wpengine.com/">WP Engine</a>.</p>
+        <p>Recently I spend most of my days playing with <a href="http://wordpress.org/">WordPress</a>,&nbsp;<a href="http://nodejs.org/">Node.js</a>, and <a href="https://reactjs.org">React</a>.</p>
+      </div>
       <LastGitHubProject username="octalmage" />
     </div>
   );
@@ -20,10 +26,6 @@ export const pageQuery = graphql`
         title
         subtitle
       }
-    }
-    wordpressPage(slug: {eq: "home"}){
-     title
-     content
     }
   }
 `;
