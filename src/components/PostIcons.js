0e8432ea977d6d33c049919320421bce0e747acc
Jason Stallings <jason@stallin.gs>
2018-08-08 19:40:22 -0500

    Fix middle align.

--- src/components/PostIcons.js
+++ src/components/PostIcons.js
@@ -6,6 +6,7 @@ import {
   FaTag as TagIcon,
   FaFolderOpen as OpenIcon,
 } from 'react-icons/fa';
+import { IconContext } from 'react-icons';
 import { rhythm } from '../utils/typography';
 
 const TaxSpan = styled.span`
@@ -15,41 +16,43 @@ const TaxSpan = styled.span`
 `;
 
 export default ({ node, className = '' }) => (
-  <div style={{ marginTop: rhythm(-1 / 2) }} className={className}>
-    <TaxSpan>
-      <ClockIcon size={14} style={{ position: 'relative', bottom: 1 }} />
-      {' '}
-      {node.date}
-    </TaxSpan>
-    {node.categories
-      && node.categories.map(category => (
-        <TaxSpan key={category.name}>
-          {' '}
-          <OpenIcon size={14} style={{ position: 'relative', bottom: 1 }} />
-          {' '}
-          <Link
-            to={`/category/${category.name.toLowerCase()}`}
-            href={`/category/${category.name.toLowerCase()}`}
-          >
-            {category.name}
-          </Link>
-        </TaxSpan>
-      ))}
-    {node.tags
-      && node.tags.map(tag => (
-        <TaxSpan key={tag.name}>
-          {'  '}
-          <TagIcon size={14} style={{ position: 'relative', bottom: 1 }} />
-          {' '}
-          <Link
-            to={`/tag/${tag.name.toLowerCase()}`}
-            href={`/tag/${tag.name.toLowerCase()}`}
-          >
-            {tag.name}
-          </Link>
-        </TaxSpan>
-      ))}
-  </div>
+  <IconContext.Provider value={{ style: { verticalAlign: 'middle' } }}>
+    <div style={{ marginTop: rhythm(-1 / 2) }} className={className}>
+      <TaxSpan>
+        <ClockIcon size={14} style={{ position: 'relative', bottom: 1 }} />
+        {' '}
+        {node.date}
+      </TaxSpan>
+      {node.categories
+        && node.categories.map(category => (
+          <TaxSpan key={category.name}>
+            {' '}
+            <OpenIcon size={14} style={{ position: 'relative', bottom: 1 }} />
+            {' '}
+            <Link
+              to={`/category/${category.name.toLowerCase()}`}
+              href={`/category/${category.name.toLowerCase()}`}
+            >
+              {category.name}
+            </Link>
+          </TaxSpan>
+        ))}
+      {node.tags
+        && node.tags.map(tag => (
+          <TaxSpan key={tag.name}>
+            {'  '}
+            <TagIcon size={14} style={{ position: 'relative', bottom: 1 }} />
+            {' '}
+            <Link
+              to={`/tag/${tag.name.toLowerCase()}`}
+              href={`/tag/${tag.name.toLowerCase()}`}
+            >
+              {tag.name}
+            </Link>
+          </TaxSpan>
+        ))}
+    </div>
+  </IconContext.Provider>
 );
 
 export const query = graphql`
